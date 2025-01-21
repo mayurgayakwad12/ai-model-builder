a9261b928b3a8e2fcd64f0d6668266d4474780e1
Mayur Gayakwad <mayurnamu11@gmail.com>
2025-01-22 00:18:21 +0530

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>AI Model Builder</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4F46E5",
+        secondary: "#1E1B4B",
+      },
+    },
+  },
+  plugins: [],
+};
